bd423c9954134cb94ed7f1a2c522fffade15e6ee
Matt H <matt.haff@gmail.com>
2022-07-24 15:48:06 -0400

    add HashRouter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 import { App } from "./App";
 import reportWebVitals from "./reportWebVitals";
 
@@ -7,9 +8,11 @@ const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <HashRouter>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
