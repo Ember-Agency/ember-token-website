ed5640eab94602eff646cab843f44291cc8b0df4
Matt H <matt.haff@gmail.com>
2022-09-16 19:58:42 -0400

    add redirect for launchpad

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import { Footer, Header, Home } from "./layout";
 import "./styles/App.scss";
 
 export const App = () => {
+  useEffect(() => {
+    // redirect user to BlockStar Launchpad
+    if (window.location.hostname === "claim.embertoken.io") {
+      window.location =
+        "https://launchpad.blockstar.site/ember-presale/?chainid=56";
+    }
+  }, []);
+
   return (
     <main className="app">
       <Header />
