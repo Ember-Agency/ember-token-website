46b43423822e7a715bc917fd8b64cdbe3d13863a
Matt H <matt.haff@gmail.com>
2022-09-18 11:42:30 -0400

    adjust colors

--- src/content/Chart.jsx
+++ src/content/Chart.jsx
@@ -12,7 +12,7 @@ export const Chart = () => {
         Buy Ember
       </a>
       <iframe
-        src="https://teams.bogged.finance/embeds/chart?address=0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c&chain=bsc&charttype=candles&theme=bg:000000FF|bg2:1f2021FF|primary:0089ffFF|secondary:1BC870FF|text:ffffffFF|text2:ffffffFF|candlesUp:1BC870FF|candlesDown:fb4a25FF|chartLine:018CF0FF&defaultinterval=15m&showchartbutton=false"
+        src="https://teams.bogged.finance/embeds/chart?address=0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c&chain=bsc&charttype=candles&theme=bg:000000FF|bg2:1f2021FF|primary:F7931EFF|secondary:00000000|text:ffffffFF|text2:ffffffFF|candlesUp:1ABE7CFF|candlesDown:fb4a25FF|chartLine:018CF0FF&defaultinterval=15m&showchartbutton=false"
         frameBorder="0"
         height="100%"
         width="100%"
