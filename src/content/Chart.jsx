da8321aadb7fd711fc74cb05a1c6eebe0bea1a8c
Matt H <matt.haff@gmail.com>
2022-09-18 17:55:10 -0400

    updating chart and buy buttons

--- src/content/Chart.jsx
+++ src/content/Chart.jsx
@@ -4,7 +4,7 @@ export const Chart = () => {
   return (
     <div className="chart-container">
       <a
-        href="https://swap.embertoken.io"
+        href="https://launchpad.blockstar.site/swap/"
         target="_blank"
         rel="noreferrer"
         className="ember-buy-btn"
@@ -12,7 +12,7 @@ export const Chart = () => {
         Buy Ember
       </a>
       <iframe
-        src="https://teams.bogged.finance/embeds/chart?address=0x36a8fcb1f8bca02dc74eb34281de3b9143eae8e3&chain=bsc&charttype=candles&theme=bg:000000FF|bg2:1f2021FF|primary:F7931EFF|secondary:00000000|text:ffffffFF|text2:ffffffFF|candlesUp:1ABE7CFF|candlesDown:fb4a25FF|chartLine:018CF0FF&defaultinterval=15m&showchartbutton=false"
+        src="https://teams.bogged.finance/embeds/chart?address=0x36a8fcb1f8bca02dc74eb34281de3b9143eae8e3&chain=bsc&charttype=candles&theme=bg:000000FF|bg2:1f2021FF|primary:F7931EFF|secondary:00000000|text:ffffffFF|text2:ffffffFF|candlesUp:1ABE7CFF|candlesDown:fb4a25FF|chartLine:018CF0FF&defaultinterval=1m&showchartbutton=false"
         frameBorder="0"
         height="100%"
         width="100%"
