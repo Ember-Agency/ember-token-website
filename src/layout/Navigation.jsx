5c707835f7f8654898876a19ba68037a9bd00bbf
Matt H <matt.haff@gmail.com>
2022-09-17 20:02:24 -0400

    updating navigation to work for dashboard > home

--- src/layout/Navigation.jsx
+++ src/layout/Navigation.jsx
@@ -11,37 +11,37 @@ export const Navigation = ({ onNavigate }) => {
     <nav>
       <ul>
         <li>
-          <Link to="#rewards" onClick={handleClick}>
+          <Link to="/#rewards" onClick={handleClick}>
             Rewards
           </Link>
         </li>
         <li>
-          <Link to="#whitepaper" onClick={handleClick}>
+          <Link to="/#whitepaper" onClick={handleClick}>
             Whitepaper
           </Link>
         </li>
         <li>
-          <Link to="#tokenomics" onClick={handleClick}>
+          <Link to="/#tokenomics" onClick={handleClick}>
             Tokenomics
           </Link>
         </li>
         <li>
-          <Link to="#security" onClick={handleClick}>
+          <Link to="/#security" onClick={handleClick}>
             Security
           </Link>
         </li>
         <li>
-          <Link to="#recovery" onClick={handleClick}>
+          <Link to="/#recovery" onClick={handleClick}>
             Recovery
           </Link>
         </li>
         {/* <li>
-          <Link to="#education" onClick={handleClick}>
+          <Link to="/#education" onClick={handleClick}>
             Education
           </Link>
         </li> */}
         <li className="hide">
-          <Link to="#contact" onClick={handleClick}>
+          <Link to="/#contact" onClick={handleClick}>
             Contact
           </Link>
         </li>
